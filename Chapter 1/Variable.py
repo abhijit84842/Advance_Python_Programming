3b8fb015c231ded04905c52370dfce2c347c7f66
Abhijit Das <abhijitd721201@gmail.com>
2023-09-14 12:02:26 +0530

    Add variable file

---
+++ Chapter 1/Variable.py
@@ -0,0 +1,10 @@
+num=2           # interger type 
+print(num)
+
+name= "Abhijit Das"     #Character Type
+print(name)
+
+# Snake case  & cammel case
+
+user_one_name= "sikha das"  #snake case writing
+userOneName="Abhijit Das"  #Cammel case writting
