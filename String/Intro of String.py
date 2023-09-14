02d9ea8dd02ecac682093b7ac6e9b4eb6bae36dd
Abhijit Das <abhijitd721201@gmail.com>
2023-09-14 23:03:39 +0530

    String

---
+++ String/Intro of String.py
@@ -0,0 +1,6 @@
+first_name="Abhijit"
+last_name="Das"
+full_name=first_name + last_name     
+print(full_name *3)     # we can multiply a number with string 
+
+print(full_name + 3)  # we can't add a number with string
