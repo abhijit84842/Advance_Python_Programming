b1f0c39329dbf7888121e9f9067626c085693d2d
Abhijit Das <abhijitd721201@gmail.com>
2023-09-14 23:11:07 +0530

    lstrip & rstrip method

---
+++ String/Strip method in string.py
@@ -0,0 +1,6 @@
+name="  Abhijit "
+dot="......"
+print(name + dot)
+print(name.lstrip() + dot)      # To remove the left side space we use lstrip()
+
+print(name.rstrip() + dot)  # To remove the right side space we use rstrip()
