93ed626bbce7412189f3585856cafeb48603890f
Abhijit Das <abhijitd721201@gmail.com>
2023-09-15 17:50:52 +0530

    Version of string formating

--- String/String_Formating.py
+++ String/String_Formating.py
@@ -7,5 +7,6 @@ print("Hello" + name + "Your age is " + str(age))  # use in python 2
 
 print("hello{} your age {}" .format(name,age +2))  # use in python 3
 
-# using f string
+# using f string in python 3.6 version
 print(f"Hello {name} Your age is {age}")  # use in python 3.6
+
