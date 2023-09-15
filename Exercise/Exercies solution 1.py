4ab5cc7e44502a4f69c70c154367c437e4ecd974
Abhijit Das <abhijitd721201@gmail.com>
2023-09-15 18:14:35 +0530

    Three number avarage find

---
+++ Exercise/Exercies solution 1.py
@@ -0,0 +1,13 @@
+# Take the integer input from the user & find out the avarage
+
+num1=int(input("Enter the first number : "))
+num2=int(input("Enter the secound number : "))
+num3=int(input("Enter the third number : "))
+
+int(num1) + int(num2) + int(num3)
+
+print(f"The avarage is {(int(num1) + int(num2) + int(num3)) /3}")
+
+
+
+
