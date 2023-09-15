243e5ed1f324d797d7000ec9a83f001e345cd4fa
Abhijit Das <abhijitd721201@gmail.com>
2023-09-15 17:40:44 +0530

    Add some file

---
+++ String/String_Formating.py
@@ -0,0 +1,11 @@
+
+name ="Abhijit"
+
+age=23
+
+print("Hello" + name + "Your age is " + str(age))  # use in python 2
+
+print("hello{} your age {}" .format(name,age +2))  # use in python 3
+
+# using f string
+print(f"Hello {name} Your age is {age}")  # use in python 3.6
---
+++ String/String_indexing.py
@@ -0,0 +1,5 @@
+
+# print only "h" from the string 
+
+name="Abhijit Das"
+print(name[2])
