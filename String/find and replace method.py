0cb4e2fd7648089be20c69c6b01bca50ddeb3c7c
Abhijit Das <abhijitd721201@gmail.com>
2023-09-15 17:56:03 +0530

    Find and replace a string

---
+++ String/find and replace method.py
@@ -0,0 +1,12 @@
+# replace() method
+# find() method
+
+
+# find()
+str1="He is a bad boy bt good in coding"
+print(str1.find("bad"))
+
+# replace()
+
+str2="She is a good girl"
+print(str2.replace("is" , "was"))
