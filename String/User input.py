4526a0d948b99c4b8f0c959d7c75917ca56e53da
Abhijit Das <abhijitd721201@gmail.com>
2023-09-15 18:05:35 +0530

    take user input from user

---
+++ String/User input.py
@@ -0,0 +1,6 @@
+# take a input from the user
+
+user_name=input("Enter your name : ")
+pass_out=int(input("Enter your college passout year : "))
+
+print(f"Name is {user_name} & passout year is {pass_out}")
