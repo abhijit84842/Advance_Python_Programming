5646af757565cb04aeb2e53860cb421477bde9d9
Abhijit Das <abhijitd721201@gmail.com>
2023-09-20 17:58:50 +0530

    Update OR operator

--- Operators in Python.py/AND ,OR Operator.py
+++ Operators in Python.py/AND ,OR Operator.py
@@ -7,3 +7,12 @@ if user_name == "admin" and password == "admin":
     print("login successfully")
 else:
     print("Not matching")
+
+# OR Operator..
+name=input("enter your name : ")
+age=int(input("enter your age : "))
+
+if name=="aditya" or age==18:
+    print("you are eligible..")
+else:
+    print("not eligible..")
