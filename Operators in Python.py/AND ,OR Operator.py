4458ea686ff66bb2a44378354fdc78c4c884105a
Abhijit Das <abhijitd721201@gmail.com>
2023-09-20 17:51:55 +0530

    AND , OR Operators

---
+++ Operators in Python.py/AND ,OR Operator.py
@@ -0,0 +1,9 @@
+# AND Operator..
+
+user_name=input("Enter your name : ")
+password=(input("Enter Your age : "))
+
+if user_name == "admin" and password == "admin":
+    print("login successfully")
+else:
+    print("Not matching")
