63b41f2ad2657f113bc8ee52ea4791857f388cde
Abhijit Das <abhijitd721201@gmail.com>
2023-09-21 17:31:21 +0530

    Upload some new file

---
+++ If else and Loop/in keyword.py
@@ -0,0 +1,8 @@
+# membership operator
+
+college="Techno India"
+
+if "o" in college:
+    print("Charecter Present")
+else:
+    print("Not present")
---
+++ If else and Loop/pass statement.py
@@ -0,0 +1,11 @@
+'''
+A statements cannot be empty, but if you for
+some reason have an if statement with no
+content, put in the pass statement to avoid
+getting an error.
+'''
+x=21
+if x > 18:
+    pass
+else:
+    print("not eligible..")
