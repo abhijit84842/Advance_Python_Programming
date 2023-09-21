483fa88eedf69139b1f335b24df901d78670c785
Abhijit Das <abhijitd721201@gmail.com>
2023-09-21 18:33:19 +0530

    Add a new program

--- Function/Function Practice.py
+++ Function/Function Practice.py
@@ -2,3 +2,12 @@
 def func(s):
     return s[-1]
 print(func("Abhijit"))
+
+# FIND EVEN AND ODD NUMBER
+
+def even_odd(num):
+    if num%2==0:
+        return "Even"
+    else:
+        return "Odd"
+print(even_odd(4))
