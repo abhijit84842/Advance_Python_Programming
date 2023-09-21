7f13c90cc96b561df2c81e3665fa5eee94ba7087
Abhijit Das <abhijitd721201@gmail.com>
2023-09-21 18:05:38 +0530

    Adding two string using function

---
+++ Function/add two string.py
@@ -0,0 +1,7 @@
+first_name=input("Enter the first name : ")
+last_name=input("Enter the last name : ")
+
+def add_name(a,b):
+    return a+b
+full_name=add_name(first_name,last_name)
+print(full_name)
