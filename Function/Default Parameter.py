9cc9971aa57bf2a301c16581bb920e210dedb9d7
Abhijit Das <abhijitd721201@gmail.com>
2023-09-21 18:15:21 +0530

    Default parameter in function

---
+++ Function/Default Parameter.py
@@ -0,0 +1,6 @@
+
+def user_info(first_name,last_name,age=23):    # This is default parameter...
+    print(f"your fname is {first_name}")        # we can use default parameter at last of parameter
+    print(f"your lname is {last_name}")
+    print(f"your age is {age}")
+user_info("abhijit" , "das")
