72f9ef64c7f3974453987881bb228dd6344395a1
Abhijit Das <abhijitd721201@gmail.com>
2023-09-22 11:52:29 +0530

    is VS equal

---
+++ List/is VS equal.py
@@ -0,0 +1,9 @@
+fruits1=["apple","kiwi","banana","orenge","grapes","apple"]
+fruits2=["apple","kiwi","banana"]
+fruits3=["apple","kiwi","banana","orenge","grapes","apple"]
+
+print(fruits1==fruits3)     
+#Output :- True (Because values are same)
+
+print(fruits1 is fruits3)
+#Output :- False (Becuse fruits1 and fruits3 object are not same place in memory.)
