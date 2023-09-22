33d903d52d60538613c6e9f71551e2dcabf1e793
Abhijit Das <abhijitd721201@gmail.com>
2023-09-22 11:34:04 +0530

    List inside list or Nested list

---
+++ List/Nested list.py
@@ -0,0 +1,21 @@
+matrix=[[1,2,3] , [4,5,6] , [7,8,9]] # 2d list..
+
+for i in matrix:
+    print(i)
+'''
+Output:-
+[1, 2, 3]
+[4, 5, 6]
+[7, 8, 9]
+
+'''
+
+# print any item of list..
+print(matrix[1])
+
+#Output:-[4, 5, 6]
+
+# access any numbers in list..
+print(matrix[2][1])
+
+#Output:- 8
