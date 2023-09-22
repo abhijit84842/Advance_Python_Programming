393f63f5d666a8e1c1dd8b668c13b4d6eba0f37c
Abhijit Das <abhijitd721201@gmail.com>
2023-09-22 11:48:20 +0530

    method join and split

---
+++ List/Join and split method.py
@@ -0,0 +1,15 @@
+# slpit method ..
+# convert string into a list..
+
+user_info="abhijit das".split(" ")
+print(user_info)
+#Output:- ['abhijit', 'das']
+
+
+# join method..
+# convert list into a string..
+
+user_info=["abhijit","21"]
+user1=("$".join(user_info))
+print(user1)
+#Output:- abhijit$21
