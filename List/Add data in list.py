2f84017d78b268ac24c346064e80110852a37fb9
Abhijit Das <abhijitd721201@gmail.com>
2023-09-22 11:07:15 +0530

    Adding data using append method

---
+++ List/Add data in list.py
@@ -0,0 +1,9 @@
+fruits=["apple" , "banana"]
+fruits.append("kiwi")       #using append method..
+print(fruits)
+
+food=[]
+food.append("rice")
+food.append("fish")
+food.append("chicken")
+print(food)
