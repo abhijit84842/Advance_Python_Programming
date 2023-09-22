223192555c854829821bca34910fa513be4f72b6
Abhijit Das <abhijitd721201@gmail.com>
2023-09-22 11:24:38 +0530

    Access list data using loop

---
+++ List/Loop in list.py
@@ -0,0 +1,11 @@
+#  for loop in list..
+
+fruits=["orange" , "banana" , "grapes"]
+for i in fruits:
+    print(i)
+
+# while loop in list
+i=0
+while i<=len(fruits):
+    print(fruits[i-1])
+    i=i+1
