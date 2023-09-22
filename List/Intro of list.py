9bdc638d3ed664d9f5c7f38740f46571a36871f5
Abhijit Das <abhijitd721201@gmail.com>
2023-09-22 11:03:54 +0530

    intro of list

---
+++ List/Intro of list.py
@@ -0,0 +1,36 @@
+# List is a data structure.
+# list is ordered collection of items..
+# you can store anything in lists int, float,string.
+
+# We create our list using square brackets[]
+
+# IMPORTANT LIST METHOD IN PYTHON
+
+#append()
+#copy()
+#count()
+#clear()
+#insert()
+#index()
+#reverse()
+#remove()
+#extend()
+#sort()
+#pop()
+
+
+
+
+number=[1,2,3,4,5]
+print(number)
+
+word=["abhi","jit","das"]
+print(word)
+
+mixed=[1,2,3,"four","five",2.5]
+print(mixed)
+
+# You can change any data on your list..
+mixed=[1,2,3,'four',2.16]
+mixed[1]='two'
+print(mixed)
