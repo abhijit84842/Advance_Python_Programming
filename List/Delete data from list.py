9ebe1c26479d0d8289c42c45e8b349da81852b53
Abhijit Das <abhijitd721201@gmail.com>
2023-09-22 11:18:20 +0530

    Various method to del data from list

---
+++ List/Delete data from list.py
@@ -0,0 +1,28 @@
+
+# Using pop method
+fruits=["apple","kiwi","banana","orenge","grapes"]
+fruits.pop()    # it will delete from last
+print(fruits)
+
+#Output:- ['apple', 'kiwi', 'banana', 'orenge']
+
+# pop method using position number..
+college=['Techno','Jis','MAKAUT','Techono International']
+college.pop(1)      # it will delete index 1
+print(college)
+
+#Output:-['Techno', 'MAKAUT', 'Techono International']
+
+# Using remove method..
+name=['Abhijit','Soumen','Ravi']
+name.remove('Soumen')
+print(name)
+
+#Output:-['Abhijit', 'Ravi']
+
+#Using dell operator..
+laptop=['hp','dell','apple','lenovo']
+del laptop[2]
+print(laptop)
+
+#Output:-['hp', 'dell', 'lenovo']
