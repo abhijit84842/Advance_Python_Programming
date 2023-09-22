b9938c5ffb782e96dc3972b1cee1e28acfec7697
Abhijit Das <abhijitd721201@gmail.com>
2023-09-22 12:01:49 +0530

    Generate list using range function

---
+++ List/More about list.py
@@ -0,0 +1,21 @@
+# generate list with range function.
+# something more about pop method..
+# index method..
+# pass list to a function..
+
+# generate list with range function.
+
+numbers=list(range(1,11))
+print(numbers)
+
+# something more about pop method..
+
+numbers=list(range(1,11))
+var=(numbers.pop())
+print(var)
+
+# index method.
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(numbers.index(3))
+
