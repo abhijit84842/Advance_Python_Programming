80be568c1530d1803d43e366262f97a721cfc412
Abhijit Das <abhijitd721201@gmail.com>
2023-09-22 11:57:31 +0530

    Grater and gratest number using min and max method

---
+++ List/min and max method.py
@@ -0,0 +1,9 @@
+numbers=[100,10,25,50,200]
+print(max(numbers))
+print(min(numbers))
+
+# find the gratest numbers from the list..
+def gratest(l):
+    return max(l)-min(l)
+numbers1=[100,60,300,500,40,20]
+print(gratest(numbers1))
