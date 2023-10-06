d27e3220549425bc0147d93aedfb90d5ec28bfc8
Abhijit Das <abhijitd721201@gmail.com>
2023-10-06 17:07:48 +0530

    intro of zip

---
+++ All type of Function/Zip function.py
@@ -0,0 +1,16 @@
+# zip function..
+
+user_info=['user1' , 'user2' ,'user3']
+names=['abhijit' , 'sikha' ,'samir']
+last_name=['sen' ,'das' ,'das']
+
+print(list(zip(user_info,names,last_name)))                 # convert in list
+
+print(tuple(zip(user_info,names,last_name)))              # convert in tuple
+
+
+# convert in dictionaries..
+information=['user1' , 'user2' ,'user3']
+names1=['abhijit' , 'sikha' ,'samir']
+
+print(dict(zip(information,names1)))
