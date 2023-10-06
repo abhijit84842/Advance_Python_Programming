be8d3140112ef17db5f24c20e17851d2c918abbe
Abhijit Das <abhijitd721201@gmail.com>
2023-10-06 11:51:44 +0530

    Intro of kwargs

---
+++ Args and Kwargs/Kwargs.py
@@ -0,0 +1,15 @@
+# kwargs = keyword argument..
+# **kwargs(double star operator..)
+# type of kwargs(dict)
+
+def func(**kwargs):
+    for k,v in kwargs.items():
+        print(f"{k} : {v}")
+
+d={
+    'name' : 'Abhijit',
+    'last name' : 'Das',
+    'Roll_no' : 27
+}
+
+print(func(**d))
