d84e7b6a56a9c4c42753d7ac15ee7eeeddcec051
Abhijit Das <abhijitd721201@gmail.com>
2023-10-06 18:25:22 +0530

    doc string explanation

---
+++ All type of Function/Doc string.py
@@ -0,0 +1,12 @@
+# What are doc string.
+# how to write docstring
+# how to see docstring
+# waht is help function
+
+def add(a,b):
+    '''this function take two numbers and return their sum'''   # this is the doc string
+    return a+b
+print(add.__doc__)  # to see the doc string..
+
+print(help(sum))   # to see the help any function..
+
