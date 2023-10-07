2334c2402c57e53bb8a68bc2282566d02bb0b722
Abhijit Das <abhijitd721201@gmail.com>
2023-10-07 13:02:32 +0530

    completed generator problem

--- Generator/Generator example.py
+++ Generator/Generator example.py
@@ -3,8 +3,12 @@
 
 def nums(n):
     for i in range(1,n+1):
-        yield i
-
+        yield i                     # NOTE:- yield is a keyword so you dont have to write i
+                                        # in parenthesis beacuse yield its not function
+                                        # i mean you can also write 'yield i' instead of, 
+                                        #  'yield(i)'
+                                
+ad=nums(10) 
 ad=nums(10)
 for numbers in ad:
     print(numbers)
