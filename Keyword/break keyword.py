d024610611e935d10ba11debeab0649bf085d8ce
Abhijit Das <abhijitd721201@gmail.com>
2024-01-05 17:13:15 +0530

    update

--- Keyword/break keyword.py
+++ Keyword/break keyword.py
@@ -1,3 +1,7 @@
+'''The break is a keyword in python which is used to bring the program control out of the loop. The break statement breaks the loops one by one, i.e., in the case of nested loops, it breaks the inner loop first and then proceeds to outer loops. In other words, we can say that break is used to abort the current execution of the program and the control goes to the next line after the loop.'''
+
+
+
 my_list=list(range(1,21))
 for item in my_list:
     # if item matched
