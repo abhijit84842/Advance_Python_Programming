471b6fc5bb4b1233e58e2ee85ec46eed8cf6379b
Abhijit Das <abhijitd721201@gmail.com>
2024-01-05 16:14:09 +0530

    loop break

---
+++ Keyword/break keyword.py
@@ -0,0 +1,22 @@
+my_list=list(range(1,21))
+for item in my_list:
+    # if item matched
+    if item==11:
+        print("Successfully Matched Item")
+        break
+    print(item)
+#print(my_list)
+
+'''
+Output=> 1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Successfully Matched Item
+'''
