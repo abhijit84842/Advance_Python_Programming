f47267ac3e7a906b17b1b834b38e5415a908d163
Abhijit Das <abhijitd721201@gmail.com>
2024-01-05 17:11:59 +0530

    first commit

---
+++ Keyword/Pass Statement.py
@@ -0,0 +1,10 @@
+# The pass statement is also known as the null statement.
+#  The pass might be utilized when we wish no code to be executed.
+
+#ex=>1
+def func(a):
+    pass
+print(func(2))
+
+
+
