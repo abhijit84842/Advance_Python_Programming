b1a5007f1d2efde9bba6a72024487c1b69ec421c
Abhijit Das <abhijitd721201@gmail.com>
2024-01-05 12:45:48 +0530

    first commit

---
+++ Literal/Python Literal.py
@@ -0,0 +1,18 @@
+# Python Literals can be defined as data that is given in a variable or constant.
+# There are many type of literals => 1)list type 2) int 3)float 4) complex 5)Decimal 6)octal etc 7) tuple
+
+x = 0b10100 #Binary Literals  
+y = 100 #Decimal Literal   
+z = 0o215 #Octal Literal  
+u = 0x12d #Hexadecimal Literal  
+  
+#Float Literal  
+float_1 = 100.5   
+float_2 = 1.5e2  
+  
+#Complex Literal   
+a = 5+3.14j  
+  
+print(x, y, z, u)  
+print(float_1, float_2)  
+print(a, a.imag, a.real)  
