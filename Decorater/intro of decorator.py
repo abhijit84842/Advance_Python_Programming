a901a111703837057d9de3a73741b4508d447f7f
Abhijit Das <abhijitd721201@gmail.com>
2024-01-06 12:20:45 +0530

    add some point

--- Decorater/intro of decorator.py
+++ Decorater/intro of decorator.py
@@ -1,4 +1,6 @@
 # decorators-enhance the functionality of other function.. video =169
+# In Decorators, functions are passed as an argument into another function and then called inside the wrapper function
+# It is also called meta programming where a part of the program attempts to change another part of program at compile time.
 #  @ use for decorator
 
 
