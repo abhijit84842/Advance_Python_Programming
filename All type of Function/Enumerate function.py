d353c33516e31f85312e7f5ea8cda4eaf1c187ee
Abhijit Das <abhijitd721201@gmail.com>
2024-01-06 17:10:24 +0530

    add more example

--- All type of Function/Enumerate function.py
+++ All type of Function/Enumerate function.py
@@ -1,4 +1,6 @@
 # We use enumerate function with for loop to track position of our item initerable..
+# Python enumerate() function returns an enumerated object.
+
 
 # how we can do this without enumerate function...
 names=['abhijit' ,'subhajit' ,'sikha' ,'samir', 'raja']
@@ -16,3 +18,22 @@ for i in names:
 # Using enumerate function..
 for pos , i in enumerate(names):
     print(f"{pos}....>{i}")
+
+
+
+# ex=> 2
+    # create a list of fruits  
+fruits = ['apple', 'banana', 'cherry']  
+  
+# iterate over the list using the enumerate() function  
+# the enumerate() function returns a tuple containing the index and value of each element  
+for pos, i in enumerate(fruits):  
+    # print the index and value of each element  
+    print(pos, i)
+    
+
+
+# ex=> 3
+numbers = [1, 2, -3, 4, -5, 6]  
+for pos, i in enumerate(numbers):
+    print(pos, i)
