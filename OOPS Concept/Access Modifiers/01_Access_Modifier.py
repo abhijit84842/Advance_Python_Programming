d0b85d20eb47f261771f0bb120fbad2059b7c051
Abhijit Das <abhijitd721201@gmail.com>
2024-01-31 19:05:54 +0530

    update

--- OOPS Concept/Access Modifiers/01_Access_Modifier.py
+++ OOPS Concept/Access Modifiers/01_Access_Modifier.py
@@ -1,3 +1,5 @@
+
+# Private Modifire -> Use Double Underscore(__)
 class Employee:
     def __init__(self):
         self.__name="Abhijit Das"               # To make Private use __
@@ -8,3 +10,24 @@ e1=Employee()
 
 
 print(e1._Employee__name)   # Also it call Name Mangling       # but can be access it indirectly.
+
+
+
+# Protected -> use Single UnderScore(_)
+class Student:
+    def __init__(self):
+        self.name1="Aditya Sen"
+
+    def _NickName(self):
+        return "Rintu"
+    
+
+class Subject(Student):
+    pass
+
+
+obj1=Student()
+obj2=Subject()
+
+print(obj1.name1)
+print(obj2._NickName())
