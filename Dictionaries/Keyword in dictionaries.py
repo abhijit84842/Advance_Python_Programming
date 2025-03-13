5c10daf7084a121b2e60d1d1c9cdcf9365a0edcc
Abhijit Das <abhijitd721201@gmail.com>
2025-03-13 18:21:48 +0530

    add a new program

--- Dictionaries/Keyword in dictionaries.py
+++ Dictionaries/Keyword in dictionaries.py
@@ -13,10 +13,18 @@ if "name" in user_info:
 else:
     print("No")
 
+# Example - 1
 # Check if value exit in dictionaries...
 if 'abhijit' in user_info.values():
     print("Present")
 else:
     print("Not present")
+
+# Example - 2
+# check if value exits in list of values.
+if "coco" in user_info["fav_movie"]:
+    print("yes")
+else:
+    print("no")
 
     
