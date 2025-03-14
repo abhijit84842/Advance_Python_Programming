fcc855cb097f0fa34078d18aad443b2319909a62
Abhijit Das <abhijitd721201@gmail.com>
2025-03-14 11:51:07 +0530

    add a new comment

--- Lambda expression/Lambda.py
+++ Lambda expression/Lambda.py
@@ -5,6 +5,8 @@
 
 # We use lambda expression with built in function..like map,reduce,filter etc.
 
+# We can use if else with lambda function but we can't use for loop directly with lambda function,intead, we canuse list comprehensions or map(for iteration)
+
 # lambda arguments: expression    
 
 def add(a,b):
